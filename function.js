298eebeaceeaf5d015c5617a5a07a57cd8eda1e4
jaspinder grewal <jgrewal40@gmail.com>
2023-08-15 18:59:46 -0700

    addes boxes for car pics

---
+++ function.js
@@ -0,0 +1,9 @@
+let head = document.getElementById('tophead');
+
+function headExtend(){
+    head.classList.add('headActive');
+}
+
+function headCollapse(){
+    head.classList.remove('headActive');
+}
