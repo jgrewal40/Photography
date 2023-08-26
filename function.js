d7d2727fdb3752d3eca65bca42bd24a6837bf146
jaspinder grewal <jgrewal40@gmail.com>
2023-08-25 17:56:27 -0700

    layout complete?

--- function.js
+++ function.js
@@ -7,3 +7,22 @@ function headExtend(){
 function headCollapse(){
     head.classList.remove('headActive');
 }
+
+let naturecollapse = document.querySelectorAll(".picturecollapse");
+let natureIndex = 0;
+
+function natureHover(){
+    console.log(naturecollapse.length);
+    naturecollapse.forEach(pic => {
+        pic.classList.remove("pictureactive");
+    });
+    naturecollapse[natureIndex].classList.add("pictureactive");
+    if(natureIndex >= naturecollapse.length - 1){
+        natureIndex = 0;
+    }
+    else{
+        natureIndex++;
+    }
+    console.log("jatt");
+}
+setInterval(natureHover, 2000);
