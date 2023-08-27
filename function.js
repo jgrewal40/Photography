5afc93736f3e59b2d2957ccd3150e1018d3e5611
jaspinder grewal <jgrewal40@gmail.com>
2023-08-27 02:34:55 -0700

    added API intersectional observer

--- function.js
+++ function.js
@@ -1,51 +1,98 @@
 let head = document.getElementById('tophead');
 
-function headExtend(){
+function headExtend() {
     head.classList.add('headActive');
 }
 
-function headCollapse(){
+function headCollapse() {
     head.classList.remove('headActive');
 }
+
+
+
+
+
+
+
+
+
+
 
 let naturecollapse = document.querySelectorAll(".picturecollapse");
 let natureIndex = 0;
 
-function natureHover(){
+function natureHover() {
     naturecollapse.forEach(pic => {
         pic.classList.remove("pictureactive");
     });
     naturecollapse[natureIndex].classList.add("pictureactive");
-    if(natureIndex >= naturecollapse.length - 1){
+    if (natureIndex >= naturecollapse.length - 1) {
         natureIndex = 0;
     }
-    else{
+    else {
         natureIndex++;
     }
 }
 setInterval(natureHover, 2000);
 
+
+
+
+
+
+
+
+
+
+
+
 const screenpics = ['assets/frame.png', 'assets/secondoption.png'];
 let screenIndex = 0;
 
-function changepic(direction){
- if(direction == 'left'){
-    if(screenIndex == 0){
-        screenIndex = screenpics.length - 1;
+function changepic(direction) {
+    if (direction == 'left') {
+        if (screenIndex == 0) {
+            screenIndex = screenpics.length - 1;
+        }
+        else {
+            screenIndex--;
+        }
     }
-    else{
-    screenIndex--;
+    if (direction == 'right') {
+        if (screenIndex == screenpics.length - 1) {
+            screenIndex = 0;
+        }
+        else {
+            screenIndex++;
+        }
     }
- }
- if(direction == 'right'){
-    if(screenIndex == screenpics.length - 1){
-        screenIndex = 0;
-    }
-    else{
-    screenIndex++;
-    }
- }
-let currentpic = document.getElementById("framepic");
-currentpic.src = screenpics[screenIndex];
-console.log(screenIndex);
+    let currentpic = document.getElementById("framepic");
+    currentpic.src = screenpics[screenIndex];
+    console.log(screenIndex);
 }
+
+
+
+
+
+
+
+const cars = document.querySelectorAll('.leftpic');
+const carsl = document.querySelectorAll('.rightpic');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+    })
+},
+{
+    threshold: 0.3,
+}
+)
+
+cars.forEach(car => {
+    observer.observe(car);
+})
+carsl.forEach(carl =>{
+    observer.observe(carl);
+})
